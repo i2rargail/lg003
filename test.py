f274450cbd1389574811a358d9bb2ca746f9682b
zhangsan <999@qq.com>
2019-04-11 20:29:09 +0800

    第五次修改

--- test.py
+++ test.py
@@ -1,9 +1,40 @@
-num1=10
+import threading
+import os
+
+def copyfile(srcpath,dstpath):
+    with open(srcpath,'rb') as r:
+        with open(dstpath,'wb') as w:
+            w.write(r.read())
 
 
+def copydir(src_dirpath,dst_dirpath):
+    filelist=os.listdir(src_dirpath)
+    if len(filelist)==0:
+        if not os.path.exists(dst_dirpath):
+            os.makedirs(dst_dirpath)
+            return
+    for i in filelist:
+        srcpath=src_dirpath + '/' + i
+        dstpath=dst_dirpath + '/' + i
+        if os.path.isfile(srcpath):
+            if not os.path.exists(dst_dirpath):
+                os.makedirs(dst_dirpath)
+            th1=threading.Thread(target=copyfile,args=(srcpath,dstpath))
+            th1.start()
+            th1.join()
 
-num2=20
+        elif os.path.isdir(srcpath):
+            copydir(srcpath,dstpath)
 
-num3=300
-num3=30
-num4=400
+def main():
+    src_dirpath=input('输入需要备份的文件路径:')
+    if not os.path.exists(src_dirpath):
+        print('输入有误...')
+        return
+    dst_dirpath=input('输入存放备份文件的位置:')
+    th2=threading.Thread(target=copydir,args=(src_dirpath,dst_dirpath))
+    th2.start()
+    print("copy完成...")
+
+if __name__=='__main__':
+    main()
